7e3da7a76920fddea08a0584693c7bab9250566e
Elang89 <elang8899@gmail.com>
2015-11-04 14:32:51 -0600

    commit inicial

---
+++ HadoopWebService/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HadoopWebService
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ HadoopWebService/Controllers/LogsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace HadoopWebService.Controllers
+{
+    public class LogsController : Controller
+    {
+        // POST: HadoopRequest
+        [HttpPost]
+        public ContentResult Create(string json)
+        {
+            Stream req = Request.InputStream;
+            req.Seek(0, System.IO.SeekOrigin.Begin);
+            string request = new StreamReader(req).ReadToEnd();
+
+
+            System.Diagnostics.Debug.WriteLine(request);
+            ContentResult response = new ContentResult { Content = json, ContentType = "application/json" };
+            return response;
+        }
+    }
+}
