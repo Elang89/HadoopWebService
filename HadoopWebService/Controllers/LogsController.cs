1ef2c0f482007132a5b2a57b053623751f8b2564
Elang89 <elang8899@gmail.com>
2015-11-05 01:07:47 -0600

    insercion de datos a hive

--- HadoopWebService/Controllers/LogsController.cs
+++ HadoopWebService/Controllers/LogsController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
+using System.Data.Odbc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+
 
 namespace HadoopWebService.Controllers
 {
@@ -15,14 +17,32 @@ namespace HadoopWebService.Controllers
         [HttpPost]
         public ContentResult Create(string json)
         {
+            OdbcConnection hiveConnection = new OdbcConnection("DSN=Hadoop Server;UID=hadoop;PWD=hadoop");
+            hiveConnection.Open();
+            System.Diagnostics.Debug.WriteLine(hiveConnection.ServerVersion);
+
             Stream req = Request.InputStream;
-            req.Seek(0, System.IO.SeekOrigin.Begin);
+            req.Seek(0, SeekOrigin.Begin);
             string request = new StreamReader(req).ReadToEnd();
-
-
             System.Diagnostics.Debug.WriteLine(request);
-            ContentResult response = new ContentResult { Content = json, ContentType = "application/json" };
-            return response;
+            ContentResult response;
+            string query;
+
+            try
+            {
+                query = "INSERT INTO TABLE error_log (json_error_log) VALUES('" + request + "')";
+                OdbcCommand command = new OdbcCommand(query, hiveConnection);
+                command.ExecuteNonQuery();
+                command.CommandText = query;
+                response = new ContentResult { Content = "{status: 1}", ContentType = "application/json" };
+                return response;
+            }
+            catch(Exception error)
+            {
+                response = new ContentResult { Content = "{status: 0, message:" + error.Message.ToString()+ "}" };
+                System.Diagnostics.Debug.WriteLine(error.ToString());
+                return response;
+            }
         }
     }
 }
