997e350a6021649f56ca8ea71ef1ff2e28fd2e12
Elang89 <elang8899@gmail.com>
2015-11-06 16:36:46 -0600

    cambios a webservice

--- HadoopWebService/Controllers/LogsController.cs
+++ HadoopWebService/Controllers/LogsController.cs
@@ -19,12 +19,10 @@ namespace HadoopWebService.Controllers
         {
             OdbcConnection hiveConnection = new OdbcConnection("DSN=Hadoop Server;UID=hadoop;PWD=hadoop");
             hiveConnection.Open();
-            System.Diagnostics.Debug.WriteLine(hiveConnection.ServerVersion);
 
             Stream req = Request.InputStream;
             req.Seek(0, SeekOrigin.Begin);
             string request = new StreamReader(req).ReadToEnd();
-            System.Diagnostics.Debug.WriteLine(request);
             ContentResult response;
             string query;
 
@@ -35,12 +33,14 @@ namespace HadoopWebService.Controllers
                 command.ExecuteNonQuery();
                 command.CommandText = query;
                 response = new ContentResult { Content = "{status: 1}", ContentType = "application/json" };
+                hiveConnection.Close();
                 return response;
             }
             catch(Exception error)
             {
                 response = new ContentResult { Content = "{status: 0, message:" + error.Message.ToString()+ "}" };
                 System.Diagnostics.Debug.WriteLine(error.ToString());
+                hiveConnection.Close();
                 return response;
             }
         }
